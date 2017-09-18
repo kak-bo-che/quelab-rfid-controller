91ee8ae7cf48dfd1d36b5a59d3a7006cb467787b
Troy Ross <kak.bo.che@gmail.com>
2017-09-17 18:39:19 -0600

    update to send unlock message

--- controller.py
+++ controller.py
@@ -3,17 +3,24 @@ from simple_hdlc import HDLC
 import time
 import serial
 import codecs
+import json
 
 ser = serial.Serial('/dev/ttyUSB0')
 
 def frame_callback(data):
-    print(data)
+    print("Received: ", data.decode('utf-8'))
+    message = json.loads(data.decode('utf-8'))
+    if message['message'] == 'rfid_card':
+        unlock_door()
 
 h = HDLC(ser, little_endian=True)
 
-
 h.startReader(onFrame=frame_callback)
 
+def unlock_door():
+    command = {"message": "lock_ctrl", "unlock": True}
+    h.sendFrame( codecs.encode(json.dumps(command)))
+    print("Sending: ", json.dumps(command))
+
 while True:
-    time.sleep(5)
-    h.sendFrame( codecs.encode('{"message":"lock_ctrl", "unlock":true}', 'ascii'))
+    time.sleep(1)
