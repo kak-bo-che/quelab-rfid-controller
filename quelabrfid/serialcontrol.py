4cefc4d28dab76b752d22af3e49be7c0772f0ed9
Troy Ross <kak.bo.che@gmail.com>
2017-09-30 13:15:21 -0600

    adding cached logins, network error handling

--- quelabrfid/serialcontrol.py
+++ quelabrfid/serialcontrol.py
@@ -1,14 +1,17 @@
+from datetime import datetime
 import time
-import serial
 import codecs
 import json
 import logging
+import pickle
+
+import serial
 from queue import Queue, Empty
 from quelabrfid.wildapricot import WildApricotApi
 from simple_hdlc import HDLC
 
 class SerialControl():
-    def __init__(self, serial_path, api_key=None, log_level=logging.INFO):
+    def __init__(self, serial_path, api_key=None, cached_logins=None, log_level=logging.INFO):
         self.serial_port = serial.Serial(serial_path)
         self.serial_connection = HDLC(self.serial_port, little_endian=True)
         self.last_rfid_time = time.monotonic()
@@ -16,6 +19,8 @@ class SerialControl():
         self.serial_connection.queue = self.queue
         self.configure_logging(log_level)
         self.last_status = {}
+        self.login_path = cached_logins
+        self.cached_logins = self._load_cached_login_file()
 
         if api_key:
             self.wa_api = WildApricotApi(api_key)
@@ -63,6 +68,8 @@ class SerialControl():
         self.stop()
 
     def frame_callback(self, data):
+        # Messages are not processed here directly, they are placed on a queue
+        # so that message processing is quick
         self.logger.debug("Received: {}".format(data.decode('utf-8')))
         message = json.loads(data.decode('utf-8'))
         if message['message'] == 'rfid_card':
@@ -72,31 +79,47 @@ class SerialControl():
         else:
             self.queue.put(message)
 
-    def _is_active_member(self, contact):
-        for field in contact['FieldValues']:
-             if field['FieldName'] == 'Membership status':
-                 status = field['Value'].get('Value')
-        if status in ['Lapsed', 'Active']:
-            return True
-        else:
-            return False
+    def _load_cached_login_file(self):
+        logins = []
+        try:
+            with open(self.login_path, 'rb') as login_file:
+                logins = pickle.load(login_file)
+
+        except (TypeError, EOFError, FileNotFoundError):
+            # None is not a valid path
+            pass
+        return logins
+
+    def _dump_cached_login_file(self):
+        with open(self.login_path, 'wb') as logins:
+            pickle.dump(self.cached_logins, logins)
+
+    def _check_cached_logins(self, rfid):
+        return next((login[1] for login in self.cached_logins if login[0] == rfid), None)
+
+    def _update_cached_logins(self, rfid, user_name):
+        # remove if rfid already accepted
+        self.cached_logins = list(filter(lambda x: x[0] != rfid, self.cached_logins))
+        self.cached_logins.insert(0, (rfid, user_name, datetime.now()))
+
+        self.cached_logins = self.cached_logins[0:10] # limit to last 10 entries
+        self._dump_cached_login_file()
+        self.logger.info(self.cached_logins)
 
     def process_message(self, message):
+        # Asyncronous Message Processor
         if message['message'] == 'rfid_card':
-            try:
-                contact = self.wa_api.find_contact_by_rfid(message['rfid'])
-                if self._is_active_member(contact):
-                    self.unlock_door(contact['DisplayName'])
-                else:
-                    self.access_denied(contact['DisplayName'])
-            except IndexError:
-                self.logger.warn("Attempted entry with unknown RFID: {}".format(message['rfid']))
+            self.rfid_received(message)
         elif message['message'] == 'status':
             self.status_received(message)
 
     def status_received(self, message):
         # {"message":"status","door_open":false,"locked":true,"lock_open":false}
         status = "Door: "
+        if self.wa_api.connected:
+            connected = "(Network Connected)"
+        else:
+            connected = "(Network Disconnected)"
         if message['door_open'] == True:
             status = status + "open, "
         else:
@@ -111,10 +134,38 @@ class SerialControl():
             status = status + "(Lock signaled)"
 
         if self.last_status == message:
-            self.logger.debug("Status: {}".format(status))
+            self.logger.debug("{} Status: {}".format(connected,status))
         else:
-            self.logger.info("Status: {}".format(status))
+            self.logger.info("{} Status: {}".format(connected, status))
         self.last_status = message
+
+    def rfid_received(self, message):
+        try:
+            contact = self.wa_api.find_contact_by_rfid(message['rfid'])
+            if self._is_active_member(contact):
+                self.unlock_door(contact['DisplayName'])
+                self._update_cached_logins(message['rfid'], contact['DisplayName'])
+            else:
+                self.access_denied(contact['DisplayName'])
+
+        except IndexError:
+            self.logger.warn("(Network Connected) Unknown RFID: {}".format(message['rfid']))
+
+        except TypeError:
+            cached_login = self._check_cached_logins(message['rfid'])
+            if cached_login:
+                self.unlock_door(cached_login)
+            else:
+                self.logger.warn("(Network Disconnected) Unknown RFID: {}".format(message['rfid']))
+
+    def _is_active_member(self, contact):
+        for field in contact['FieldValues']:
+            if field['FieldName'] == 'Membership status':
+                status = field['Value'].get('Value')
+        if status in ['Lapsed', 'Active']:
+            return True
+        else:
+            return False
 
     def access_denied(self, user_name):
         self.logger.info("Access denied to: {}".format(user_name))
@@ -122,5 +173,8 @@ class SerialControl():
     def unlock_door(self, user_name):
         command = {"message": "lock_ctrl", "unlock": True}
         self.serial_connection.sendFrame( codecs.encode(json.dumps(command)))
-        self.logger.info("Opening door for: {}".format(user_name))
+        if self.wa_api.connected:
+            self.logger.info("(Network Connected) Opening door for: {}".format(user_name))
+        else:
+            self.logger.info("(Network Disconnected) Opening door for: {}".format(user_name))
         self.logger.debug("Sending: {}".format(json.dumps(command)))
--- quelabrfid/wildapricot.py
+++ quelabrfid/wildapricot.py
@@ -1,28 +1,39 @@
 import json
 import os
 import urllib.parse
+from urllib.error import URLError
 from wildapricotapi import ApiException, WaApiClient
 
 class WildApricotApi(object):
     def __init__(self, api_key):
         self.api = WaApiClient(api_key=api_key)
-        self.api.authenticate_with_apikey()
-        accounts = self.api.execute_request("/v2/accounts")
-        self.account = accounts[0]
+        self.account = None
+        self.connected = False
+        self.authenticate()
 
-    def find_contact_by_id(self, id):
-        contactsUrl = next(res for res in self.account['Resources'] if res['Name'] == 'Contacts')['Url']
-        params = {'$async': 'false'}
-        request_url = contactsUrl + str(id) +  '?' + urllib.parse.urlencode(params)
-        response = self.api.execute_request(request_url)
+    def authenticate(self):
+        try:
+            if not self.api.get_access_token():
+                self.api.authenticate_with_apikey()
+                accounts = self.api.execute_request("/v2/accounts")
+                self.account = accounts[0]
+                self.connected = True
+        except URLError:
+            self.connected = False
 
     def find_contact_by_rfid(self, rfid):
+        self.authenticate()
         contact = None
-        contactsUrl = next(res for res in self.account['Resources'] if res['Name'] == 'Contacts')['Url']
-        params = {'$async': 'false', '$filter': "RFID eq " + str(rfid)}
+        if self.account:
+            contactsUrl = next(res for res in self.account['Resources'] if res['Name'] == 'Contacts')['Url']
+            params = {'$async': 'false', '$filter': "RFID eq " + str(rfid)}
 
-        request_url = contactsUrl +  '?' + urllib.parse.urlencode(params)
-        response = self.api.execute_request(request_url)
-        if response:
-            contact = response['Contacts'][0]
+            request_url = contactsUrl +  '?' + urllib.parse.urlencode(params)
+            try:
+                response = self.api.execute_request(request_url)
+                if response:
+                    contact = response['Contacts'][0]
+                self.connected = True
+            except URLError:
+                self.connected = False
         return contact
